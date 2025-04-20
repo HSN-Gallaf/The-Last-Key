4ad4123fc10fe791b120ba61d9e57aa5f24f7cdf
HSN-Gallaf <hheamg2005@gmail.com>
2025-04-20 21:57:36 +0300

    locomotion for sprint, crouch, walk and jump
    i created locomotion for sprint, crouch, walk
    and added a jumping functinality.

--- Assets/Scripts/PlayerMovment.cs
+++ Assets/Scripts/PlayerMovment.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class PlayerMovment : MonoBehaviour
 {
@@ -10,31 +9,87 @@ public class PlayerMovment : MonoBehaviour
 
     public float gravity = -9.81f;
     public float speed = 5f;
+    public float crouchSpeed = 2f;
+    public float sprintSpeed = 8f;
+    public float jumpHeight = 2f;
+
     public CharacterController controller;
-    public Animator animator; // Assign this in the Inspector if it's not on the same GameObject
+    public Animator animator;
+
+    private bool isCrouching = false;
+    private bool isSprinting = false;
+    private bool isGrounded;
+    private bool isJumping = false;
+
+    public bool IsCrouching => isCrouching;
+    public bool IsGrounded => isGrounded;  // Add a getter to access the ground state
 
     void Update()
     {
+        // Input
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
-
         move = transform.right * x + transform.forward * z;
 
-        // Set animation state using "IsWalking" (capital I)
-        bool IsWalking = move.magnitude > 0.1f;
-        animator.SetBool("IsWalking", IsWalking);
-        Debug.Log("IsWalking: " + IsWalking);
+        // Ground check
+        isGrounded = controller.isGrounded;
+        animator.SetBool("isGrounded", isGrounded);
 
-        // Apply gravity
-        if (!controller.isGrounded)
+        // Falling check
+        if (!isGrounded && velocity.y < 0)
         {
-            velocity.y += gravity * Time.deltaTime;
+            animator.SetBool("isFalling", true);
         }
         else
         {
-            velocity.y = -2f;
+            animator.SetBool("isFalling", false);
         }
 
-        controller.Move((move * speed + velocity) * Time.deltaTime);
+        // Landing logic
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+            isJumping = false;
+            animator.SetBool("isJumping", false);
+        }
+
+        // Jump input
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            // Stand up before jumping
+            if (isCrouching)
+            {
+                isCrouching = false;
+                animator.SetBool("isCrouching", false);
+            }
+
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);  // Apply jump velocity
+            isJumping = true;
+            animator.SetBool("isJumping", true);
+        }
+
+        // Sprint logic (only forward movement)
+        isSprinting = Input.GetKey(KeyCode.LeftShift) && z > 0 && !isCrouching;
+        animator.SetBool("isSprinting", isSprinting);
+
+        // Crouch toggle
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            isCrouching = !isCrouching;
+            animator.SetBool("isCrouching", isCrouching);
+        }
+
+        // Set speed based on movement type
+        float currentSpeed = isCrouching ? crouchSpeed : isSprinting ? sprintSpeed : speed;
+
+        // Pass movement to Animator
+        animator.SetFloat("Horizontal", x);
+        animator.SetFloat("Vertical", z);
+
+        // Apply gravity
+        velocity.y += gravity * Time.deltaTime;
+
+        // Apply movement
+        controller.Move((move * currentSpeed + velocity) * Time.deltaTime);
     }
 }
